ef7a9977ad042ba517333c591742360e16203eef
Laura Robertson <mslaura.mae@gmail.com>
2018-04-15 00:23:38 -0700

    Bar and skills

--- styles/application.js
+++ styles/application.js
@@ -1,10 +1,24 @@
-$(window).scroll(function(){
-    if ($(window).scrollTop() >= 470) {
-       $('header').addClass('fixed-header');
-       $('#about p').addClass('fixed-adjustment');
-    }
-    else {
-       $('header').removeClass('fixed-header');
-       $('#about p').removeClass('fixed-adjustment');
-    }
+$(window).scroll(() => {
+  if ($(window).scrollTop() >= 475) {
+    $('header').addClass('fixed-header');
+    $('#about p').addClass('fixed-adjustment');
+  } else {
+    $('header').removeClass('fixed-header');
+    $('#about p').removeClass('fixed-adjustment');
+  }
+});
+
+(function($) {
+  $.fn.progress = function() {
+    var percent = this.data("percent");
+    this.css("width", percent + "%");
+  };
+}(jQuery));
+
+$(document).ready(() => {
+  $('.bar1').progress();
+  $('.bar2').progress();
+  $('.bar3').progress();
+  $('.bar4').progress();
+  $('.bar5').progress();
 });
